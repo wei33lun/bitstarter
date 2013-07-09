50ecad4c9ba9ccb6e5255f6e841d501080957c07
Victor Chen <victorc@gmail.com>
2013-07-09 05:16:42 +0000

    Fixed missing paren

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync("index.html");
+var buf = new Buffer(fs.readFileSync("index.html"));
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
