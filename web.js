3be226d4143220c367e497ef626fcc6251ab18dc
v chen <ubuntu@ip-172-31-46-159.us-west-2.compute.internal>
2013-07-09 05:09:39 +0000

    Updated to index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
