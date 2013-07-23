a6460e66cacb268af79fc10a90000446bf8483ec
wei33lun <victorc@gmail.com>
2013-07-23 05:48:06 +0000

    edited product desc in index.html

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync("index.html"));
+var htmlfile = "index.html";
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);  
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
